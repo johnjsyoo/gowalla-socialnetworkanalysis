061b22865c78d2cbb468120fb9c5d11ec34e92c2
John Yoo <johnjsyoo@gmail.com>
2016-02-04 16:33:01 -0500

    Updated R File

--- gowalla.R
+++ gowalla.R
@@ -1,2 +1,24 @@
 library(readr)
 
+setwd("~/GitHub/gowalla-socialnetworkanalysis")
+
+# Loading Data -- Make sure file names match and the working directory is set
+
+edgeList <- read_delim("gowalla_edges.txt", col_names = FALSE)
+
+fullData <- read.delim("gowalla_totalCheckins.txt", sep = "\t")
+
+
+fullData <- read.table("gowalla_totalCheckins.txt", sep = "\t")
+
+edgeListCols <- c("Node_1",
+                  "Node_2")
+
+fullDataCols <- c("user",
+                  "check-in_time",	
+                  "latitude",
+                  "longitude",
+                  "location id")
+
+colnames(edgeList) <- edgeListCols
+colnames(fullData) <- fullDataCols
